bebec0b7e58027f1bd5f5d1b07151d9ca7e02401
Andrew Sun <andy.sun00@gmail.com>
2015-04-18 21:52:22 +0000

    WP-KaTeX version 1.3.0

--- scripts/frontend.php
+++ scripts/frontend.php
@@ -7,9 +7,9 @@ function katex_init() {
 
 	if (is_array($katex_options)) {
 		if (isset($katex_options['katex_jsdelivr_setting']) && $katex_options['katex_jsdelivr_setting'] == 1) {
-      // TODO: Fix jsDelivr links
-			wp_register_style('katex', '//cdn.jsdelivr.net/katex/0.1.1/katex.min.css', false, null);
-			wp_register_script('katex', '//cdn.jsdelivr.net/katex/0.1.1/katex.min.js', array('jquery'), null);
+			// TODO: Fix jsDelivr links
+			wp_register_style('katex', '//cdn.jsdelivr.net/katex/0.3.0/katex.min.css', false, null);
+			wp_register_script('katex', '//cdn.jsdelivr.net/katex/0.3.0/katex.min.js', array('jquery'), null);
 		} else {
 			wp_register_style('katex', plugins_url('assets/katex.min.css', dirname(__FILE__)), false, '0.3.0');
 			wp_register_script('katex', plugins_url('assets/katex.min.js', dirname(__FILE__)), array('jquery'), '0.3.0', true);
@@ -24,28 +24,36 @@ add_action('init', 'katex_init');
 function katex_handler($atts, $content = null){
 	wp_enqueue_script('katex');
 	wp_enqueue_style('katex');
-	$break = null;
-	if (strpos($content, '\\displaystyle') === 0) {
-		return '<p><script type="text/katex">' . html_entity_decode($content) . '</script></p>';
+	// $break = null; //?
+	$latex_atts = shortcode_atts( array(
+		'display' => 'false',
+	), $atts );
+
+	if ($latex_atts['display'] == 'true') {
+		return '<script type="text/katex" display="true">' . html_entity_decode($content) . '</script>';
 	} else {
-		return '<script type="text/katex">' . html_entity_decode($content) . '</script>';
+		return '<script type="text/katex" display="false">' . html_entity_decode($content) . '</script>';
 	}
 }
 add_shortcode('latex', 'katex_handler');
 
 function katex_rubber() {
-	?>
-	<script>
-		(function($) {
-			$("script[type='text/katex']").each(function (i) {
-				try {
+?>
+<script>
+	(function($) {
+		$("script[type='text/katex']").each(function (i) {
+			try {
+				if (this.attributes.display.textContent == "true") {
+					katex.render(this.text, $("<span></span>").insertAfter(this)[0], { displayMode: true });
+				} else {
 					katex.render(this.text, $("<span></span>").insertAfter(this)[0]);
-				} catch (e) {
-					$("<span style='color:red'>" + e + "</span>").insertAfter(this);
 				}
-			});
-		})(jQuery);
-	</script>
-	<?php
+			} catch (e) {
+				$("<span style='color:red'>" + e + "</span>").insertAfter(this);
+			}
+		});
+	})(jQuery);
+</script>
+<?php
 }
 add_action('wp_footer', 'katex_rubber', 100);
