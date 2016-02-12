79a407fe930548f56c1e84cedf11256e74bc0885
Andrew Sun <as-com@users.noreply.github.com>
2016-02-11 20:49:42 -0500

    Merge in random fixes I made on my own server

--- scripts/frontend.php
+++ scripts/frontend.php
@@ -1,5 +1,6 @@
 <?php
 global $katex_options;
+global $katex_using;
 $katex_options = get_option('katex_settings');
 
 function katex_init() {
@@ -8,7 +9,7 @@ function katex_init() {
 	if (is_array($katex_options)) {
 		if (isset($katex_options['katex_jsdelivr_setting']) && $katex_options['katex_jsdelivr_setting'] == 1) {
 			wp_register_style('katex', '//cdn.jsdelivr.net/katex/0.5.1/katex.min.css', false, null);
-			wp_register_script('katex', '//cdn.jsdelivr.net/katex/0.5.1/katex.min.js', array('jquery'), null);
+			wp_register_script('katex', '//cdn.jsdelivr.net/katex/0.5.1/katex.min.js', array('jquery'), null, true);
 		} else {
 			wp_register_style('katex', plugins_url('assets/katex.min.css', dirname(__FILE__)), false, '0.5.1');
 			wp_register_script('katex', plugins_url('assets/katex.min.js', dirname(__FILE__)), array('jquery'), '0.5.1', true);
@@ -21,8 +22,10 @@ function katex_init() {
 add_action('init', 'katex_init');
 
 function katex_handler($atts, $content = null){
+	global $katex_using;
 	wp_enqueue_script('katex');
 	wp_enqueue_style('katex');
+	$katex_using = true;
 	// $break = null; //?
 	$latex_atts = shortcode_atts( array(
 		'display' => 'false',
@@ -37,24 +40,27 @@ function katex_handler($atts, $content = null){
 add_shortcode('latex', 'katex_handler');
 
 function katex_rubber() {
-?>
-<script>
-	(function($) {
-		$("script[type='text/katex']").each(function (i) {
-			try {
-				console.log(this);
-				if (this.attributes.display.textContent == "true") {
-					katex.render(this.text, $("<span></span>").insertAfter(this).get(0), { displayMode: true });
-				} else {
-					katex.render(this.text, $("<span></span>").insertAfter(this).get(0));
+	global $katex_using;
+	if ($katex_using) {
+	?>
+	<script>
+		(function($) {
+			$("script[type='text/katex']").each(function (i) {
+				try {
+					console.log(this);
+					if (this.attributes.display.textContent == "true") {
+						katex.render(this.text, $("<span></span>").insertAfter(this).get(0), { displayMode: true });
+					} else {
+						katex.render(this.text, $("<span></span>").insertAfter(this).get(0));
+					}
+				} catch (e) {
+					$("<span style='color:red'>" + e + "</span>").insertAfter(this);
+					console.log(e);
 				}
-			} catch (e) {
-				$("<span style='color:red'>" + e + "</span>").insertAfter(this);
-				console.log(e);
-			}
-		});
-	})(jQuery);
-</script>
-<?php
+			});
+		})(jQuery);
+	</script>
+	<?php
+	}
 }
 add_action('wp_footer', 'katex_rubber', 100);
